e0c7ca9dbab6cefc22ff23bc182aaee7e646f2b6
Alyssa Crawford <alyssacrawford@Alyssas-MacBook-Pro.local>
2022-06-17 16:15:37 -0700

    Added more descriptive plot labels

--- 01-modeling.R
+++ 01-modeling.R
@@ -1,29 +1,29 @@
 # load libraries
 library(tidyverse)
-library(lubridate)
+#library(lubridate)
 library(effects)
 
 # read in data
 compas_data <- read_csv('https://raw.githubusercontent.com/propublica/compas-analysis/master/compas-scores-raw.csv')
 
 # filter to include only Risk of Violence
-compas_data_rov <- compas_data %>% 
+compas_data_rov <- compas_data %>%
   filter(DisplayText == 'Risk of Violence',
          DecileScore > -1)
 
 # mutate to add years and correct dates before 1969
-compas_data_rov <- compas_data_rov %>% 
+compas_data_rov <- compas_data_rov %>%
   mutate(Screening_Date = as.Date(Screening_Date, format="%m/%d/%y"),
          DateOfBirth = as.Date(DateOfBirth, format="%m/%d/%y"))
 
-compas_data_rov$DateOfBirth[format(compas_data_rov$DateOfBirth, format = "%Y") > 2010] <- 
+compas_data_rov$DateOfBirth[format(compas_data_rov$DateOfBirth, format = "%Y") > 2010] <-
   compas_data_rov$DateOfBirth[format(compas_data_rov$DateOfBirth, format = "%Y") > 2010] - years(100)
 
 # add age column
-compas_data_rov <- compas_data_rov %>% 
+compas_data_rov <- compas_data_rov %>%
   mutate(age = as.period(interval(compas_data_rov$DateOfBirth, compas_data_rov$Screening_Date))$year)
-           
-# make linear model comparing age and risk of violence score      
+
+# make linear model comparing age and risk of violence score
 age_rov_model <- lm(DecileScore ~ age,
                     data = compas_data_rov)
 
@@ -32,29 +32,45 @@ effects_df <- effect("age", age_rov_model, xlevels=8) %>%
   data.frame()
 
 # plot effects
-effects_df %>% 
+effects_df %>%
   ggplot(aes(x = age,
              y = fit,
              ymin = lower,
              ymax = upper,
-             label = round(fit, digits=2))) + 
-  geom_errorbar() + 
-  geom_label()
+             label = round(fit, digits=2))) +
+  geom_errorbar() +
+  geom_label() +
+  labs(title = "Risk of Violence predicted by COMPAS",
+       subtitle = "Linear regression results -- score ~ age",
+       caption = "Data acquired by ProPublic (2016)",
+       x = "Age",
+       y = "Risk of Violence Decile Score")
 
-compas_data_rov %>% 
+compas_data_rov %>%
   ggplot(aes(x = age,
-             y = DecileScore)) + 
-  geom_col()
+             y = DecileScore)) +
+  geom_point() +
+  labs(title = "Risk of Violence predicted by COMPAS",
+       caption = "Data acquired by ProPublic (2016)",
+       x = "Age",
+       y = "Risk of Violence Decile Score")
 
 # create new column with age range as categorical variable
 compas_data_rov$age_groups = cut(compas_data_rov$age, breaks=c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100))
 
 # plot risk of violence scores by age
-compas_data_rov %>% 
-  ggplot(aes(x = DecileScore)) + 
-  geom_bar() + 
-  facet_wrap(~age_groups) # still need to fix labels, add title, etc
+compas_data_rov %>%
+  ggplot(aes(x = DecileScore)) +
+  geom_bar() +
+  facet_wrap(~age_groups) +
+  labs(title = "Risk of Violence predicted by COMPAS",
+       subtitle = "Score distribution by age group",
+       caption = "Data acquired by ProPublic (2016)",
+       x = "Risk of Violence Decile Score",
+       y = "Frequency")
 
-     
-         
+
+
+
+
 
